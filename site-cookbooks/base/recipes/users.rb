ca463c9e795c7ddd170483f6d9fe6c63241037b0
Chihiro SAKATOKU <csakatoku@gmail.com>
2014-03-01 19:34:04 +0900

    Change to use data_bag/users to define users

--- site-cookbooks/base/recipes/users.rb
+++ site-cookbooks/base/recipes/users.rb
@@ -8,29 +8,43 @@
 #
 include_recipe 'sudo'
 
-user 'csakatoku' do
-  uid '2005'
-  shell '/bin/bash'
-  supports :manage_home => true
-end
+data_bag('users').each do |name|
+  bag = data_bag_item('users', name)
 
-directory '/home/csakatoku' do
-  owner 'csakatoku'
-  mode 0755
-end
+  user name do
+    uid bag['uid']
+    shell bag['shell']
+    supports :manage_home => true
+  end
 
-directory '/home/csakatoku/.ssh' do
-  owner 'csakatoku'
-  mode 0700
-end
+  home = "/home/#{name}"
+  directory home do
+    owner name
+    mode 0755
+  end
 
-file '/home/csakatoku/.ssh/authorized_keys' do
-  content 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDVF97XH+gGz48EzrcmoBuqNo7KXNnz45y9brsQS2XzEA0BGum/TBNAB7lLnIspSVFbYuqXav9h0JvaTwa6Rc09NTd5f1gDUiW3XStCrOT+FaN4x7JwVIi6iaOEsBks+fJWxTdkdi5UhWXjlmodh0iFHtgeR+Ft/8XZ3h6ETB/M9hkqHrflB4G9LkpeyHmi313cNuN2x8r6+aJNkSTm1t6ymPjhBArEo9/qkoVH01C10j3XIqOJ+/GaDziAmQN972N8Fh9klhrgutZf27MVg5XGA2xx/NtqWDT+K9vMa1p1QrHH5mnUJjK5KX7QvC5wHcMBbzpTfVA27j6Se9IYBBiR5Bc0JHXIYPOZOl8p6PajERoS983UKiTTVwRB49cetNBZVWMRhbdN4Eg6snsqmsPNW0kadwZtIfCtACYaTv4PLjoS2xBiCh0t7PiynyDYElFJ0+Dr4BluEYcqDypyirzTevPpMI0lkNwfNlF1n7r1Enteww4mSwXNb/bzWd1TlkSeJsWWwAYuyqOBwlahjv8lz5L91cLNwVXD2ZxJM3WHnPOXXoYtXmcMnX+rQ2/NxHNtG9X0Yk8FAIfMRBPLtkaSxhgKyvvFBVyp+gTWWsODYZafmbQjFFJUSFi2o9F+wamRcmz/R1/4D75kQqmhvL7s8g1C7wXWvhKyZ1vJtYj1rQ=='
-  owner 'csakatoku'
-  mode 0600
-end
+  directory "#{home}/.ssh" do
+    owner name
+    mode 0700
+  end
 
-sudo 'csakatoku' do
-  user "%csakatoku"
-  nopasswd true
+  if bag['ssh_authorized_keys']
+    file "#{home}/.ssh/authorized_keys" do
+      content bag['ssh_authorized_keys'].join('\n')
+      owner name
+      mode 0600
+      action :create
+    end
+  else
+    file "#{home}/.ssh/authorized_keys" do
+      action :delete
+    end
+  end
+
+  if bag['sudo']
+    sudo name do
+      user bag['sudo']['user']
+      nopasswd bag['sudo']['nopasswd']
+    end
+  end
 end

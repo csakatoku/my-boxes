24f47576c5d8599a2141e18f126e8777ce75f1e5
Chihiro SAKATOKU <csakatoku@gmail.com>
2014-03-01 18:02:25 +0900

    Add a first chef recipe

---
+++ site-cookbooks/base/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w{zsh git tmux curl}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
